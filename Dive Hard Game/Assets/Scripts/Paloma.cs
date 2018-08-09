474543acfde55b48375e414d38c8f3a037e3111d
DSKT01 <diego99gu@hotmail.com>
2018-08-09 17:31:44 -0500

    Diego
    Estructura basica de los objetos

---
+++ Dive Hard Game/Assets/Scripts/Paloma.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paloma : InGameObjects
+{
+    protected override void Action()
+    {
+      
+    }
+
+    protected override IEnumerator Activation()
+    {
+        return base.Activation();
+    }
+}
---
+++ Dive Hard Game/Assets/Scripts/Singleton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton : MonoBehaviour
+{
+    public static Singleton _instance;
+    public static int[] storeLevels = new int[2];//se puede cambiar
+    // Use this for initialization
+    void Awake()
+    {
+        if (_instance == null)
+            _instance = this;
+        else
+            Destroy(transform.gameObject);
+    }
+}
