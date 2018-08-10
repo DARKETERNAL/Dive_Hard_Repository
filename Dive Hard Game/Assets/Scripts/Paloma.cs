354ed3212c4f36cfad4ef0d85285151456979855
duckyproductions <seb_325@hotmail.com>
2018-08-10 17:40:34 -0500

    Duck
    Paloma OP

--- Dive Hard Game/Assets/Scripts/Paloma.cs
+++ Dive Hard Game/Assets/Scripts/Paloma.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class Paloma : InGameGoodObjects
 {
-    protected override void Action() //comportamiento pasivo
+	[SerializeField]
+	float cantidadAcelerar = 2;
+
+	protected override void Action() //comportamiento pasivo
     {
       
     }
 
     protected override IEnumerator Activation(Player _jugador) //comportamiento activo
     {
-        yield return null;
-    }
+		Rigidbody2D rb = _jugador.GetComponent<Rigidbody2D>();
+		rb.velocity = new Vector2(rb.velocity.x , Mathf.Abs(rb.velocity.y * cantidadAcelerar));
+		Destroy(GetComponent<Paloma>());
+		//Aqui un sonido de paloma muriendo
+		yield return null;
+	}
 }
