de267dde7460b7532089d645b5a14bb303a65fe7
DSKT01 <diego99gu@hotmail.com>
2018-08-16 10:17:09 -0500

    paloma fix
    paloma fix

--- Dive Hard Game/Assets/Scripts/In Game Mechanics/Paloma.cs
+++ Dive Hard Game/Assets/Scripts/In Game Mechanics/Paloma.cs
@@ -6,17 +6,27 @@ public class Paloma : MundoConTienda
 {
 	[SerializeField]
 	float cantidadAcelerar = 2;
-
+    int lim = 0;
+    bool activeDelta;
 	protected override void Action() //comportamiento pasivo
     {
-      
+        if (activeDelta != active)
+        {
+            lim = 0;
+        }
+        activeDelta = active;
     }
 
     protected override IEnumerator Activation(Player _jugador) //comportamiento activo
     {
-		Rigidbody2D rb = _jugador.GetComponent<Rigidbody2D>();
-		rb.velocity = new Vector2(rb.velocity.x , Mathf.Abs(rb.velocity.y * cantidadAcelerar));
-		Destroy(GetComponent<Paloma>());
+        if (lim == 0)
+        {
+            Rigidbody2D rb = _jugador.GetComponent<Rigidbody2D>();
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Abs(rb.velocity.y * cantidadAcelerar));
+            lim++;
+        }
+		
+
 		//Aqui un sonido de paloma muriendo
 		yield return null;
 	}
