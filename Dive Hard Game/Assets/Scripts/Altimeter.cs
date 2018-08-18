709d4d21d4c538d82799df117324f8bf0ba18fdc
Josethehedgehog <preguntas.josethehedgehog@gmail.com>
2018-08-18 18:33:56 -0500

    Trip
    -Cambiado los placeholders para que correspondan con los powerups
    -Agregado sprite gigante placeholder del edificio de donde se lanza
    -Medidor de altura agregado, faltan tweaks pero ya muestra la altura del personaje en un string y se mueve el "rostro del personaje" a lo largo de un "slider"

---
+++ Dive Hard Game/Assets/Scripts/Altimeter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Altimeter : MonoBehaviour {
+
+
+    RectTransform altimeterFace;
+    Transform playerTransform;
+    TextMeshProUGUI altimeterText;
+    float currentAltitude;
+
+    
+
+	// Use this for initialization
+	void Start () {
+
+        altimeterFace = GetComponent<RectTransform>();
+        playerTransform = GameObject.Find("Player").GetComponent<Transform>();
+        altimeterText = GetComponentInChildren<TextMeshProUGUI>();
+
+        altimeterFace.localPosition = new Vector3(0, (playerTransform.position.y / 5) , 0);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float t = Time.deltaTime;
+
+        altimeterFace.localPosition = new Vector3(0, playerTransform.position.y / 5 , 0);
+        currentAltitude = Mathf.Round(playerTransform.localPosition.y);
+        altimeterText.text = string.Format("{0}m ----", currentAltitude.ToString());  
+	}
+}
