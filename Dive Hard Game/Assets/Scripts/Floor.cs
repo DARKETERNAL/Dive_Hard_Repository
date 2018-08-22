af73d45bf2735c58a03598203d9955f790cb749f
DSKT01 <diego99gu@hotmail.com>
2018-08-21 21:43:20 -0500

    no mucho

---
+++ Dive Hard Game/Assets/Scripts/Floor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Floor : MonoBehaviour {
+
+    [SerializeField]
+    Edificio alturaDeEdificio;
+    float floorHeigth;
+    Transform player;
+    private void Start()
+    {
+        player = GameObject.Find("Player").transform;
+        floorHeigth = Mathf.Abs(player.position.y - alturaDeEdificio.transform.position.y);
+    }
+    // Update is called once per frame
+    void FixedUpdate () {
+        if (player.position.y <= floorHeigth)
+        {
+            //this coud be change
+            StartCoroutine(RestartProtocole());
+        }
+	}
+    //this coud be change
+    IEnumerator RestartProtocole()
+    {
+        yield return new WaitForSeconds(5f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        yield return null;
+    }
+}
--- Dive Hard Game/Assets/Scripts/In Game Mechanics/Edificio.cs
+++ Dive Hard Game/Assets/Scripts/In Game Mechanics/Edificio.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Edificio : PassiveMechanics
 {
+    private float altura;
+    public float Altura { get { return altura; } set { altura = value * ActualLevel ;} }
     protected override void Execution()//start
     {
-      
+        Altura = 1000;
     }
 }
