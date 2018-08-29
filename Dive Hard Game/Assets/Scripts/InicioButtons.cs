b252a3023fd7c8d2ff04c2eef3cae7ad8009edf9
EstebanOlarte <estebanolarte1999@gmail.com>
2018-08-29 01:17:44 -0500

    Escena inicio

---
+++ Dive Hard Game/Assets/Scripts/InicioButtons.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InicioButtons : MonoBehaviour {
+
+    public void Jugar()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Salir()
+    {
+        Application.Quit();
+    }
+}
