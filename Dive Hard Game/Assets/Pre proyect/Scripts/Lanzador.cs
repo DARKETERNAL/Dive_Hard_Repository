64f7bc2a3431ad4eca95d52a5b84152da7570057
Josethehedgehog <preguntas.josethehedgehog@gmail.com>
2018-09-06 21:43:50 -0500

    Trip

--- Dive Hard Game/Assets/Pre proyect/Scripts/Lanzador.cs
+++ Dive Hard Game/Assets/Pre proyect/Scripts/Lanzador.cs
@@ -23,6 +23,7 @@ public class Lanzador : MonoBehaviour {
 	public bool jumping = true;
 	public bool canClic;
 	bool click;
+    public bool launched = false;
 
 	[SerializeField]
 	float velocidad, amplitud, anguloInicio;
@@ -131,6 +132,7 @@ public class Lanzador : MonoBehaviour {
 		swipe.enabled = true;
 		mText.text = 0.ToString();
 		Time.timeScale = 1;
+        launched = true;
 	}
 
 	void Apuntar()
