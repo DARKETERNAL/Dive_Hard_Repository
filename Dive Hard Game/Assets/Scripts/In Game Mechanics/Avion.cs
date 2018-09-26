342ac93e5689e9e1442d5bb424408aeba9925900
Josethehedgehog <preguntas.josethehedgehog@gmail.com>
2018-09-26 13:54:07 -0500

    Tripabosky
    Sounds and Font

--- Dive Hard Game/Assets/Scripts/In Game Mechanics/Avion.cs
+++ Dive Hard Game/Assets/Scripts/In Game Mechanics/Avion.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UberAudio;
 
 public class Avion : ObjectsParents
 {
@@ -11,7 +12,9 @@ public class Avion : ObjectsParents
 
     protected override void Action() //comportamiento pasivo
     {
-       
+        //Aqui sonido del engine del Avion???
+        //AudioManager.Instance.Play("PlaneSound");
+
     }
 
     protected override IEnumerator Activation(Player _jugador) //comportamiento activo
@@ -19,7 +22,8 @@ public class Avion : ObjectsParents
         
             Rigidbody2D rb = _jugador.GetComponent<Rigidbody2D>();
             rb.velocity = new Vector2((rb.velocity.x / cantidadFrenar), 0);
-            //Aqui un sonido de choque con avion
+        //Aqui un sonido de choque con avion
+        AudioManager.Instance.Play("MetalHit");
             //animación del choque con avion
         
         yield return null;
